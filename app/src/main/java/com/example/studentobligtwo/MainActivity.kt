3d4a9dae07a4ce6ceb53f0c92a2863b80e7e84ac
Seryn <schmitttni@gmail.com>
2022-03-02 15:33:03 +0100

    Added Empty Quiz and Database Activity Orientation Mode for Main

--- app/src/main/java/com/example/studentobligtwo/MainActivity.kt
+++ app/src/main/java/com/example/studentobligtwo/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.studentobligtwo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startQuiz(view: View) {
+        val intent = Intent(this, QuizActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun startDatabase(view: View) {
+        val intent = Intent(this, DatabaseActivity::class.java)
+        startActivity(intent)
     }
 }
